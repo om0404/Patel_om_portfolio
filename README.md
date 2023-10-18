4aee281169b7ef7fd73e736ed0e27f12effdd1a2
om0404 <o_patel164575@fanshaweonline.ca>
2023-10-18 16:27:02 -0400

    final
    final_work

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Patel_om_portfolio
+Working on portfolio
+
+## Installation
+TODO: i am still going to work on gsap and contact page as well.
+
+## Contributing:
+1. Fork it!
+2. Create your feature branch: git checkout -b my-new-feature
+3. Commit your changes: git commit -am 'Add some feature'
+4. Push to the branch: git push origin my-new-feature
+5. Submit a pull request :D
+
+## Credits
+TODO:credits goes to
+ Om Patel
+ bchiang7
+
+## License
+MIT
--- js/main.js
+++ js/main.js
@@ -2,5 +2,18 @@
     console.log("fired");
 
     let _projects =document.querySelector ("#_button");
+
+    gsap.registerpligin(scrolltoplugin);
+
+    const navlinks = document.querySelectorAll("#b-container");
+
+    function scrollLink(e){
+        e.preventdefault();
+        let selectedlink = e.currentTarget.hash;
+        gsap.to(window, {duration:1, scrollTo:{y:`${selectedlink}`, offsety: 100}});
+    }
     
+    navlinks.forEach((navlinks)=>{
+        navlinks.addEventListener("click", scrollLink);
+    });
 })();
